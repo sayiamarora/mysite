3d4f7e661f8d7d281d855f310a7977504e05f607
sayiamarora <sayiam0127@gmail.com>
2024-09-11 18:25:48 +0530

    Commit 4

--- food/views.py
+++ food/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
+
 # Create your views here.
 
 def index(request):
     list_of_items=Item.objects.all()
-
-    return HttpResponse(list_of_items)
+    template=loader.get_template('food/index.html')
+    context={
+        'list_of_items':list_of_items,
+    }
+    return HttpResponse(template.render(context,request))
 
 def item(request):
     return HttpResponse("<h1>this is an selected item</h1>")
+
+
+def detail(request,item_id):
+    item=Item.objects.get(pk=item_id)
+    context={
+        'item':item,
+    }
+    return render(request, 'food/detail.html',context)
