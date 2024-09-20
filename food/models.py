0c33afc97c47e98a289e42b2f164c6cede370bda
Sayiam <sayiam0127@gmail.com>
2024-09-21 01:04:40 +0530

    Commit 5

--- food/models.py
+++ food/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://static.vecteezy.com/system/resources/previews/010/826/341/original/vacation-icon-isolated-on-transparent-background.png")
+
     def __str__(self):
         return self.item_name
-    item_name=models.CharField(max_length=200)
-    item_desc=models.CharField(max_length=200)
-    item_price=models.IntegerField()
-    item_image=models.charField(max_length=500,default="https://static.vecteezy.com/system/resources/previews/010/826/341/original/vacation-icon-isolated-on-transparent-background-png.png")
- 
--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,9 @@
-from . import views
 from django.urls import path
+from . import views
 
-app_name='food'
-urlpatterns=[
-    path('',views.index,name='index'),
-    path('<int:item_id>/',views.detail,name='detail'),
-    path('item',views.item,name='name'),
+app_name = 'food'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('item/', views.item, name='item'),  # Fixed missing slash after 'item'
 ]
