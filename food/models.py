fc71d7156e18b138f34cf0b168d7c29a30bfff85
Sayiam <sayiam0127@gmail.com>
2024-09-20 23:25:55 +0530

    commit 5

--- food/models.py
+++ food/models.py
@@ -7,4 +7,5 @@ class Item(models.Model):
     item_name=models.CharField(max_length=200)
     item_desc=models.CharField(max_length=200)
     item_price=models.IntegerField()
+    item_image=models.charField(max_length=500,default="https://static.vecteezy.com/system/resources/previews/010/826/341/original/vacation-icon-isolated-on-transparent-background-png.png")
  
